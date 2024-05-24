2658dae864a2cc55dec8f48e0f4d4aa8e1bca599
eduardoVela <eduardovela2022@icloud.com>
2024-05-24 14:22:30 -0600

    Added inquirer questions, and added an init function to the index.js file to run the project.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const logoBot = require("./lib/logoBot");
+
+function init() {
+  logoBot();
+}
+
+init();
+
 // Resources
 //https://www.w3schools.com/graphics/svg_rect.asp
 //https://www.w3schools.com/graphics/svg_circle.asp
+//https://www.w3schools.com/graphics/svg_polygon.asp
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes/extends
